80597cd5e11ff760313a289437c661cb9d3bd108
Jordan Reaves <jordancreaves1@gmail.com>
2025-05-21 00:27:18 -0400

    created outputs

--- outputs.tf
+++ outputs.tf
@@ -1,29 +1,29 @@
 output "vpc_id" {
-  description = "VPC ID"
-  value       = aws_vpc.main.id
+    description = "VPC ID"
+    value       = aws_vpc.main.id
 }
 
 output "public_subnet_ids" {
-  description = "Public Subnet IDs"
-  value       = aws_subnet.subnet-1.*.id
+    description = "Public Subnet IDs"
+    value       = aws_subnet.subnet-1.id
 }
 
 output "internet_gateway" {
-  description = "Internet Gateway ID"
-  value       = aws_internet_gateway.igw.id
+    description = "Internet Gateway ID"
+    value       = aws_internet_gateway.igw.id
 }
 
 output "route_table" {
-  description = "Route Table ID"
-  value       = aws_route_table.public_rt.id
+    description = "Route Table ID"
+    value       = aws_route_table.public_rt.id
 }
 
 output "nginx_id" {
-  description = "Nginx Instance ID"
-  value       = aws_instance.nginx_proxy.id
+    description = "Nginx Instance ID"
+    value       = aws_instance.nginx_proxy.id
 }
 
 output "nginx_public_ip" {
-  description = "Nginx Public IPcl"
-  value       = aws_instance.nginx_proxy.public_ip
+    description = "Nginx Public IPcl"
+    value       = aws_instance.nginx_proxy.public_ip
 }
