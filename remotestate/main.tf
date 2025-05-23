71b7f7cabd599867042bb5a468ea009933812bdf
Jordan Reaves <jordancreaves1@gmail.com>
2025-05-22 23:58:34 -0400

    versioning

--- remotestate/main.tf
+++ remotestate/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "~> 1.11.4"
+  required_version = "~> 1.11.4, < 2.0.0"
 
   required_providers {
     aws = {
@@ -8,11 +8,11 @@ terraform {
     }
   }
 
-  backend "s3" {
-    bucket         = "remotestate-jreavesbucket-dev"
-    key            = "global/s3/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"         # <- This enables locking
-    encrypt        = true
-  }
+  # backend "s3" {
+  #   bucket         = "remotestate-jreavesbucket-dev"
+  #   key            = "global/s3/terraform.tfstate"
+  #   region         = "us-east-1"
+  #   dynamodb_table = "terraform-locks"         # <- This enables locking
+  #   encrypt        = true
+  # }
 }
